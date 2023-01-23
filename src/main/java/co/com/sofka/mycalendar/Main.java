1a010e70706779563d964ff7a2d8e2ea786b915a
Ivan <idruizbe@gmail.com>
2023-01-23 16:16:29 -0500

    Documentación

--- src/main/java/co/com/sofka/mycalendar/Main.java
+++ src/main/java/co/com/sofka/mycalendar/Main.java
@@ -1,9 +1,21 @@
 package co.com.sofka.mycalendar;
-
-import co.com.sofka.mycalendar.calendario.Calendario;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Calendario calendario = new Calendario();
+        do{
+
+        }while(menu()!=6);
+    }
+    public static int menu(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\nMenu:"+
+                "\n1) Crear actividad"+
+                "\n2) Ver calendario"+
+                "\n3) Ver semana"+
+                "\n4) Crear semana de actividades"+
+                "\n5) Cancelar actividad"+
+                "\n6) Salir"+
+                "\n\nSelecciona una opcion: ");
+        return sc.nextInt();
     }
 }
