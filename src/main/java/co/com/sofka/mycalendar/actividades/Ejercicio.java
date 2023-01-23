7709b1e5171049010fbb09a2ceff6694dff2ac00
SergioBalca <SergioBalca@github.com>
2023-01-23 16:09:14 -0500

    agregar clase Ejercicio que implementa las interfaces Programable y laborable y que hereda de la clase abstracta Actividad

--- src/main/java/co/com/sofka/mycalendar/actividades/Ejercicio.java
+++ src/main/java/co/com/sofka/mycalendar/actividades/Ejercicio.java
@@ -1,2 +1,23 @@
-package co.com.sofka.mycalendar.actividades;public class Ejercicio {
+package co.com.sofka.mycalendar.actividades;
+
+import co.com.sofka.mycalendar.Laborable;
+import co.com.sofka.mycalendar.Programable;
+
+import java.time.LocalDate;
+
+public class Ejercicio extends Actividad implements Programable, Laborable {
+
+    public Ejercicio(String titulo, LocalDate fechaInicial, LocalDate fechaFinal) {
+        super(titulo, fechaInicial, fechaFinal);
+    }
+
+    @Override
+    public void agregarRepeticion(int repeticiones) {
+
+    }
+
+    @Override
+    public boolean esValido() {
+        return false;
+    }
 }
