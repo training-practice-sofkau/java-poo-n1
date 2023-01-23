56f1a91808ca86f6fbf0a96d40f34fdfa267bf0e
JessiLopezObando <jessicaandrealopezobando@gmail.com>
2023-01-23 16:51:44 -0500

    Actividad Trabajar usando enum

--- src/main/java/co/com/sofka/mycalendar/DiasDeLaSemana.java
+++ src/main/java/co/com/sofka/mycalendar/DiasDeLaSemana.java
@@ -1,5 +1,12 @@
 package co.com.sofka.mycalendar;
 
 public enum DiasDeLaSemana {
-    LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO;
+
 }
