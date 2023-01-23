9cfca4aec9ef3bafadd773ae54ffa0f17878f730
Ococho <jonathan.vargas85@gmail.com>
2023-01-23 16:06:19 -0500

    Intento de implementación del calendario propuesto.

--- src/main/java/co/com/sofka/mycalendar/Main.java
+++ src/main/java/co/com/sofka/mycalendar/Main.java
@@ -1,8 +1,17 @@
 package co.com.sofka.mycalendar;
 
+import co.com.sofka.mycalendar.actividades.Ejercicio;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         Calendario calendario = new Calendario();
 
+        calendario.agregarSemana(Semana.I);
+        /* Inicio de la lógica que seguiría
+         *
+         * calendario.agregarActividad(new Ejercicio("Pecho", "2023-01-23", "2032-01-24","TrenInferior"));
+         * calendario.actividadList[0].setTipo("TrenSuperior");*/
     }
 }
