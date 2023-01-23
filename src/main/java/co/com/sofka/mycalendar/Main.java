874be3b97e6bb12eeaa851f612ab05650e7c1c8a
Eros Jose Adarraga Jimenez <dev.erosadarraga@gmail.com>
2023-01-23 14:53:25 -0500

    Enums

--- src/main/java/co/com/sofka/mycalendar/Main.java
+++ src/main/java/co/com/sofka/mycalendar/Main.java
@@ -1,9 +1,12 @@
 package co.com.sofka.mycalendar;
 
+import co.com.sofka.mycalendar.Enums.Weekdays;
+
 public class Main {
 
     public static void main(String[] args) {
         Calendario calendario = new Calendario();
+        System.out.println(Weekdays.MONDAY.toString());
     }
 
 
