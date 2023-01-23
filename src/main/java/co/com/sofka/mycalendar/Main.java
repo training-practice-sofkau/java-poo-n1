d704f582f65f07a5459874a8e024767ad46a5fb1
juan david cardona velasquez <j.cardona1@utp.edu.co>
2023-01-23 16:17:49 -0500

    Prueba con el main

--- src/main/java/co/com/sofka/mycalendar/Main.java
+++ src/main/java/co/com/sofka/mycalendar/Main.java
@@ -1,8 +1,19 @@
 package co.com.sofka.mycalendar;
 
+import co.com.sofka.mycalendar.actividades.Lectura;
+
+import java.time.LocalDate;
+
 public class Main {
 
+
     public static void main(String[] args) {
+        LocalDate date = LocalDate.parse("12-02-2020");
+        LocalDate date2 = LocalDate.parse("15-02-2020");
+        Lectura prueba = new Lectura("100 años de soledad", date, date2);
         Calendario calendario = new Calendario();
+        calendario.agregarActividad(prueba);
+        System.out.println(calendario);
     }
+
 }
